f4bf51a18130c5c1e840077fd5918d2eba3b2a1e
navetacandra <dev@navetacandraa.my.id>
2025-11-03 01:12:31 +0700

    feat: implement fsscan and diff detection

--- cmd/gowle.go
+++ cmd/gowle.go
@@ -2,27 +2,56 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/navetacandra/gowle/internal/config"
+	"github.com/navetacandra/gowle/internal/fsscan"
 	"github.com/navetacandra/gowle/internal/worker"
 )
 
 func main() {
+	verbose := flag.Bool("v", false, "Enable verbose mode. Print every change")
+	flag.Parse()
 	fmt.Println("[GOWLE] Start.")
 
 	sigs := make(chan os.Signal, 1)
 	scanner := bufio.NewScanner(os.Stdin)
 	appConfig := config.GowleConfig{}
+	snapshot := []fsscan.Info{}
+	snapshotDiff := []fsscan.DiffInfo{}
 
 	appWorker := worker.NewWorker(func() { // on start
 		appConfig.Load()
+		fsscan.Scan(&snapshot, &appConfig)
 	}, func() { // on loop
+		tmpSnapshot := []fsscan.Info{}
+		fsscan.Scan(&tmpSnapshot, &appConfig)
+		fsscan.DiffSnapshot(&tmpSnapshot, &snapshot, &snapshotDiff)
+
+		if len(snapshotDiff) > 0 {
+			snapshot = tmpSnapshot
+			if *verbose {
+				for _, d := range snapshotDiff {
+					mode := "modified"
+					switch d.Diff {
+					case -1:
+						mode = "deleted"
+					case 1:
+						mode = "created"
+					}
+					fmt.Printf("%s was %s\n", d.Path, mode)
+				}
+			}
+
+			// TODO: Implement action on change
+		}
 	}, func() { // on stop
-	})
+	}, 1000*time.Millisecond)
 
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM) // handle external termination
 	go func() {
--- internal/fsscan/fsscan.go
+++ internal/fsscan/fsscan.go
@@ -1,3 +1,120 @@
 package fsscan
 
-// TODO: implement fs scanning
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/navetacandra/gowle/internal/config"
+)
+
+type Info struct {
+	Path    string
+	ModSize int64
+	Size    int64
+}
+
+type DiffInfo struct {
+	Path string
+	Diff int8
+}
+
+func DiffSnapshot(newSnapshot *[]Info, oldSnapshot *[]Info, diff *[]DiffInfo) {
+	*diff = (*diff)[:0]
+	new := make(map[string]Info)
+	old := make(map[string]Info)
+
+	for _, f := range *oldSnapshot {
+		old[f.Path] = f
+	}
+	for _, f := range *newSnapshot {
+		new[f.Path] = f
+	}
+
+	for p, o := range old {
+		n, exist := new[p]
+		switch {
+		case !exist:
+			*diff = append(*diff, DiffInfo{Path: p, Diff: -1}) // delete
+		case o.Size != n.Size || o.ModSize != n.ModSize:
+			*diff = append(*diff, DiffInfo{Path: p, Diff: 0}) // modified
+		}
+	}
+
+	for p := range new {
+		_, exist := old[p]
+		if !exist {
+			*diff = append(*diff, DiffInfo{Path: p, Diff: 1}) // created
+		}
+	}
+}
+
+func Scan(snapshot *[]Info, cfg *config.GowleConfig) {
+	cwd, _ := os.Getwd()
+	ignored := make(map[string]int8)
+	*snapshot = (*snapshot)[:0]
+
+	filepath.WalkDir(cwd, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		name := d.Name()
+
+		if d.IsDir() {
+			if shouldIgnore(path, name, &cfg.Ignore, &ignored) {
+				ignored[path] = 1
+			}
+			return nil
+		}
+
+		if shouldIgnore(path, name, &cfg.Ignore, &ignored) {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+		relPath, _ := filepath.Rel(cwd, path)
+		if shouldWatch(&relPath, &cfg.Watch) {
+			*snapshot = append(*snapshot, Info{
+				Size:    info.Size(),
+				ModSize: info.ModTime().Unix(),
+				Path:    relPath,
+			})
+		}
+		return nil
+	})
+}
+
+func shouldWatch(path *string, prefixes *[]string) bool {
+	if len(*prefixes) == 0 {
+		return true
+	}
+
+	for _, p := range *prefixes {
+		if strings.HasPrefix(*path, p+"/") || *path == p {
+			return true
+		}
+	}
+	return false
+}
+
+func shouldIgnore(absPath, base string, patterns *[]*regexp.Regexp, ignored *map[string]int8) bool {
+	for dir := range *ignored {
+		if strings.HasPrefix(absPath, dir+"/") {
+			return true
+		}
+	}
+
+	for _, r := range *patterns {
+		if r.MatchString(base) {
+			return true
+		}
+	}
+
+	return false
+}
